3687171873260e3f998d95fa3fdede9791adbd03
meozalat2022 <meozalat2002@hotmail.com>
2023-10-31 21:06:48 +0200

    movie page

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -6,7 +6,7 @@ import { FiThumbsUp } from "react-icons/fi";
 const Card = ({ result, key }) => {
   return (
     <div className="group cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
-      <Link href={`/movies/${result.id}`}>
+      <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
